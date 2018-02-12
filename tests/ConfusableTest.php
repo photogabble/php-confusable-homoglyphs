ffb28c1c1f85096ee7dddd457846eacf03040d38
Simon Dann <carbontwelve@users.noreply.github.com>
2018-02-12 21:47:40 +0000

    Porting Test from vhf/confusable_homoglyphs

--- tests/ConfusableTest.php
+++ tests/ConfusableTest.php
@@ -2,11 +2,19 @@
 
 namespace Photogabble\ConfusableHomoglyphs\Tests;
 
+use Photogabble\ConfusableHomoglyphs\Confusable;
+
 class ConfusableTest extends Base
 {
     public function testIsMixedScript()
     {
-        // @todo
+        $confusables = new Confusable();
+        $this->assertTrue($confusables->isMixedScript($this->looksGood));
+        $this->assertTrue($confusables->isMixedScript(' ρττ a'));
+
+        $this->assertFalse($confusables->isMixedScript($this->isGood));
+        $this->assertFalse($confusables->isMixedScript('ρτ.τ'));
+        $this->assertFalse($confusables->isMixedScript('ρτ.τ '));
     }
 
     public function testIsConfusable()
@@ -16,6 +24,18 @@ class ConfusableTest extends Base
 
     public  function testIsDangerous()
     {
-        // @todo
+        $confusables = new Confusable();
+        
+        $this->assertTrue($confusables->isDangerous($this->looksGood));
+        $this->assertTrue($confusables->isDangerous(' ρττ a'));
+        $this->assertTrue($confusables->isDangerous('ρττ a'));
+        $this->assertTrue($confusables->isDangerous('Alloτ'));
+        $this->assertTrue($confusables->isDangerous('www.micros﻿оft.com'));
+        $this->assertTrue($confusables->isDangerous('www.Αpple.com'));
+        $this->assertTrue($confusables->isDangerous('www.faϲebook.com'));
+        $this->assertFalse($confusables->isDangerous('AlloΓ', ['latin']));
+        $this->assertFalse($confusables->isDangerous($this->isGood));
+        $this->assertFalse($confusables->isDangerous(' ρτ.τ'));
+        $this->assertFalse($confusables->isDangerous('ρτ.τ'));
     }
 }
