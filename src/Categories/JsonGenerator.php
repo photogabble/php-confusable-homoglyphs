3a21d40ff90ce6cf0e4bb97be4933cb15f4e0b71
Simon Dann <carbontwelve@users.noreply.github.com>
2018-02-20 14:09:23 +0000

    Line parsing complete, needs sort?

--- src/Categories/JsonGenerator.php
+++ src/Categories/JsonGenerator.php
@@ -57,14 +57,30 @@ class JsonGenerator
         if (preg_match('/Date: ([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01])), ((?:(?:([01]?\d|2[0-3]):)?([0-5]?\d):)?([0-5]?\d)) ([A-Z]+)/', $line, $dateMatches) > 0) {
             $this->sourceDatestamp = new \DateTime($dateMatches[1] . ' ' . $dateMatches[4], new \DateTimeZone($dateMatches[8]));
             return;
-        }
+        } unset($dateMatches);
 
         if (preg_match('/([0-9A-F]+)(?:\.\.([0-9A-F]+))?\W+(\w+)\s*#\s*(\w+)/', $line, $matches) < 1) {
             return;
         }
 
-        $n = 1;
+        $code_point_range_from = $matches[1];
+        $code_point_range_to = $matches[2];
+        $alias = mb_strtoupper($matches[3]);
+        $category = $matches[4];
+
+        if (! in_array($alias, $this->iso15924Aliases)){
+            $this->iso15924Aliases[] = $alias;
+        }
+
+        if (! in_array($category, $this->categories)){
+            $this->categories[] = $category;
+        }
+
+        $this->codePointsRanges[] = [
+            hexdec($code_point_range_from),
+            hexdec($code_point_range_to),
+            array_search($alias, $this->iso15924Aliases, true),
+            array_search($category, $this->categories, true)
+        ];
     }
-
-
 }
